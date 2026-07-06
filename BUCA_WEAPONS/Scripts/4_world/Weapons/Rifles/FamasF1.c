e69599fc2cef1fb286a3bbaa3b25af50feddfc8e
arbalen <samsungvanek068@gmail.com>
2026-07-06 22:06:21 +0300

    Remove all files from repository

--- BUCA_WEAPONS/Scripts/4_world/Weapons/Rifles/FamasF1.c
+++
@@ -1,8 +0,0 @@
-class BC_FAMASF1_Base : RifleBoltFree_Base
-{
-    override RecoilBase SpawnRecoilObject()
-    {
-        return new AUGRecoil(this);
-    }
-};
-class BC_FAMASF1: BC_FAMASF1_Base {};
--- IMPWMODPart2/Scripts/4_World/Sniper/SCAR20.c
+++
@@ -1,8 +0,0 @@
-class IMP_SCAR20S_Base : RifleBoltLock_Base
-{
-	override RecoilBase SpawnRecoilObject()
-	{
-		return new FALRecoil(this);
-	}
-};
-class IMP_SCAR20S : IMP_SCAR20S_Base {};
