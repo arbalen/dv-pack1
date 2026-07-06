8b13bddb10e7dbf1dae6805c365ce395fd04b001
arbalen <samsungvanek068@gmail.com>
2026-07-06 22:31:11 +0300

    Remove all files from repository

--- BUCA_WEAPONS/Scripts/4_world/Weapons/Rifles/G36.c
+++
@@ -1,9 +0,0 @@
-class BC_G36_Base : RifleBoltFree_Base
-{
-    override RecoilBase SpawnRecoilObject()
-    {
-        return new FALRecoil(this);
-    }
-};
-class BC_G36: BC_G36_Base {};
-class BC_G36_Admin: BC_G36_Base {};
--- BUCA_WEAPONS2/Scripts/4_world/Weapons/Pistols/FNFiveSeven.c
+++
@@ -1,8 +0,0 @@
-class BC_FNFiveSeven_Base : Pistol_Base
-{
-	override RecoilBase SpawnRecoilObject()
-	{
-		return new GlockRecoil(this);
-	}
-};
-class BC_FNFiveSeven_Tan : BC_FNFiveSeven_Base{};
